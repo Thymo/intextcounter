c2a4f9152b5541fd74928b7b20c827bf0cf797a8
Max Rijnierse <maxrijnierse@gmail.com>
2018-05-08 01:25:53 +0200

    Initial structure

--- src/App.js
+++ src/App.js
@@ -2,18 +2,36 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+`;
+
+const Main = styled.div`
+  Flex: 3;
+`;
+
+const SideBar = styled.div`
+  Flex: 1;
+  max-width: 400px;
+  background-color: #F8F8F8;
+  border-left: 1px solid #EEE;
+  padding: 160px 0px 0px 40px;
+`;
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Wrapper>
+        <Main>
+
+        </Main>
+        <SideBar>
+          <div> infotext</div>
+        </SideBar>
+      </Wrapper>
     );
   }
 }
