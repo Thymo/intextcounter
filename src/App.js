39dc5f231cdf590a22d58f71dd19f3eca70543bf
Max Rijnierse <maxrijnierse@gmail.com>
2018-05-08 01:49:43 +0200

    styling sidebar row Issue: missing responsiveness

--- src/App.js
+++ src/App.js
@@ -7,30 +7,44 @@ import styled from 'styled-components';
 const Wrapper = styled.div`
   height: 100vh;
   display: flex;
+  color: #333;
+  font-size: 15px;
 `;
 
 const Main = styled.div`
-  Flex: 3;
+  Flex: 4;
 `;
 
 const SideBar = styled.div`
   Flex: 1;
   max-width: 400px;
+  min-width: 250px;
   background-color: #F8F8F8;
   border-left: 1px solid #EEE;
-  padding: 160px 0px 0px 40px;
+  padding: 160px 30px 0px 40px;
+
+  @media(max-width: 600px){
+    width: 100%;
+    max-width: 100%;
+  }
 `;
 
 const Row = styled.div`
+  padding: 5px;
+  margin-bottom: 20px;
+`;
+
+const Label = styled.span`
+  float: left;
+  margin-right: 20px;
 
 `;
 
-const Label = styled.div`
-
-`;
-
-const Value = styled.div`
-
+const Value = styled.span`
+  background: white;
+  padding: 3px 10px;
+  border: 1px solid #DC143C;
+  float: right;
 `;
 
 
@@ -45,19 +59,19 @@ class App extends Component {
         <SideBar>
           <Row>
             <Label>Words</Label>
-            <Value></Value>
+            <Value>x</Value>
           </Row>
           <Row>
             <Label>Words excluding citations</Label>
-            <Value></Value>
+            <Value>y</Value>
           </Row>
           <Row>
             <Label>Inline citations</Label>
-            <Value></Value>
+            <Value>z</Value>
           </Row>
           <Row>
             <Label>Citations words</Label>
-            <Value></Value>
+            <Value>a</Value>
           </Row>
         </SideBar>
       </Wrapper>
